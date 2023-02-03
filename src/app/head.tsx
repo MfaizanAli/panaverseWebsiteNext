78d009abfe05d4bf3536ae0b43dc3fc14c9e5937
unknown <faizanmalik135@gmail.com>
2023-02-03 23:05:45 +0500

    second

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -7,7 +7,7 @@ export default function Head() {
       <link rel="icon" href="/favicon.ico" /> */}
       
        {/* new code */}
-         <meta charset="UTF-8" />
+         <meta charSet="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
